8a4282b86e46aeb8d173acbfda5e36b23bb3d4d6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 23:56:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  name_for_suite_a = passengers.suite_a
+  name_for_suite_a = passengers[:suite_a]
   if name_for_suite_a.slice(0, 1).upcase == "A"
     return name_for_suite_a
   end

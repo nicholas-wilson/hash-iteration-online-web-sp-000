20a24f795fc55883826cb8fe3d641b507ab13be0
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 00:03:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,8 +7,6 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kid, age|
-    if age <= 12
-      puts "Happy Birthday #{kid}! You are now #{age} years old!"
-    end
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end

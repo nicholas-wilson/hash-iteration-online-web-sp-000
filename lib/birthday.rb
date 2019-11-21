176f0e51881248d3f7ffb65fd9048826838e4e02
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 00:00:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each_with_index do |kid|
-    puts birthday_kids[kid]
+    puts kid
     if birthday_kids[kid] <= 12
       puts "Happy Birthday #{kid}!"
     end

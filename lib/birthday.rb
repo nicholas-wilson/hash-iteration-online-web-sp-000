a46e3b76096cdaecb111683efca6b63cf5f61a2b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 00:07:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 def age_appropriate_birthday(birthdays)
   birthdays.each do |person, age|
     if age <= 12
-      "Happy Birthday #{kid}! You are now #{age} years old!"
+      "Happy Birthday #{person}! You are now #{age} years old!"
     end
   end
 end

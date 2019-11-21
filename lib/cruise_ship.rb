0a8b1ee3d6fc57b80fe0544f0b1390ceb0e27f86
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 00:07:50 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,15 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  name_for_suite_a = passengers[:suite_a]
+  if name_for_suite_a.slice(0, 1).upcase == "A"
+    return name_for_suite_a
+  end
 end
